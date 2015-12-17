9d3743ad5f13c29d0a33a0a3a89b7126a7912b77
Nate <nate@optimizely.com>
2015-12-16 22:05:35 -0500

    still trying

--- js/index.js
+++ js/index.js
@@ -1,35 +1,48 @@
-
-
 // Globals
-var lastTime = 0
-var circleX = 200
-var circleY = 200
+var lastTime = 0;
+
+var circleX = 200;
+var circleY = 200;
+var historicCircles = [];
 
 // Init
 var setup = function() {
   // Full size of window
   createCanvas(windowWidth, windowHeight);
 
-  // Background image
+  // Load file for background image
   bg = loadImage("img/optigrey.svg");
-  background(bg);
-  console.log('hi');
 }
 
 // Render loop
 var draw = function() {
 
-  // Randomly color and display the circle
+  // Background
+  background(bg);
+
+  // Render existing circles
+  for (i = 0; i < historicCircles.length; i++) {
+    stroke(randomColor());
+    fill(randomColor());
+    ellipse(historicCircles[i].x, historicCircles[i].y, 15, 15);
+  }
+
+  // New random circle
   stroke(randomColor());
   fill(randomColor());
   ellipse(circleX, circleY, 15, 15);
+
+  var myCircle = new Object();
+  myCircle.x = circleX;
+  myCircle.y = circleY;
+  historicCircles.push(myCircle);
 
   // Move the circle to next position, or reset pos if it hits edges
   if (circleX === 0 || circleX === windowWidth || circleY === 0 || circleY === windowHeight){
     resetCirclePos()
   }
-  circleX = circleX + Math.floor(Math.random() * 4) - 1.1
-  circleY = circleY + Math.floor(Math.random() * 4) - 1.1
+  circleX = circleX + Math.floor(Math.random() * 4) - 1.1;
+  circleY = circleY + Math.floor(Math.random() * 4) - 1.1;
 
   // // Flash message every 10 seconds
   // if (millis() - lastTime >= 10000) {
